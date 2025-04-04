545f67decd0d4533c6240d44f55ba456f325f755
rucomen5978 <121585255+rucomen5978@users.noreply.github.com>
2025-04-05 01:52:23 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,454 +1,123 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <math.h>
-
-//regular digit format
-char rdf[2] = "f";
-
-//Functions
-
-int gcd(int a, int b)
-{
-  while (b != 0)
-  {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int gcd(int a, int b) {
+  while (b != 0) {
     int temp = b;
     b = a % b;
     a = temp;
   }
   return a;
 }
-
-int lcm(int a, int b)
-{
-  return (a * b) / gcd(a, b);
-}
-
-//Formul itm
-//fp = num/denom
-//num %= denom
-
-//Formul tif
-//num = denom * fp + num
-
-int addfracnum(int fn, int fd, int sn, int sd)
-{
-  int denomval = lcm(fd, sd);
-  fn *= (denomval / fd);
-  sn *= (denomval / sd);
-  return fn + sn;
-}
-
-int subfracnum(int fn, int fd, int sn, int sd)
-{
-  int denomval = lcm(fd, sd);
-  fn *= (denomval / fd);
-  sn *= (denomval / sd);
-  return fn - sn;
-}
-
-//Formul mulfrac
-//fn *= sn
-//fd *= sd
-
-//Formul divfrac
-//fn *= sd
-//fd *= sn
-
-//Formul cf
-//(snd/sdd) = (fnd/fdd)*(snn/sdn)/(fnn/fdn)
-//(tnn/tdn) = (fnn/fdn)*(tnd/tdd)/(fnd/fdd)
-
-
-int main()
-{
+int lcm(int a, int b) { return a * b / gcd(a, b); }
+int main() {
   char action[256];
-  while (1)
-  {
-    scanf("%s", &action);
-   
-    if (!strcmp(action, "exit"))
+  while (1) {
+    scanf("%s", action);
+    if (!strcmp(action, "exit") || !strcmp(action, "ex"))
       return 0;
-
-    if (!strcmp(action, "clear"))
+    if (!strcmp(action, "cl") || !strcmp(action, "clear"))
       system("clear");
-
-    if (!strcmp(action, "lof")) //list of functions
-      printf("exit clear lof crdf chrdf cf add sub mul div pow rod af sf mf df fd tdap tif itm dtf reduce rt gcd lcm\n");
-
-
-    if (!strcmp(action, "crdf")) //change regular digit format
-    {
-      scanf("%s", rdf);
-      if (!strcmp(rdf, "d"))
-        printf("%%d -> int\n");
-      else if (!strcmp(rdf, "f"))
-        printf("%%f -> float\n");
-      else if (!strcmp(rdf, "lf"))
-        printf("%%lf -> double\n");
-      else if (!strcmp(rdf, "l"))
-        printf("%%l = %%Lf -> long double\n");
-      else if (!strcmp(rdf, "Lf"))
-        printf("%%Lf -> long double\n");
-      else
-        printf("wrong format\n");
-    }
-
-    if (!strcmp(action, "chrdf"))
-      printf("%s\n", rdf);
- 
-    //cf
-    if (!strcmp(action, "cf"))
-    {
-      int fnn, fdn, fnd, fdd;
-      int snn, sdn, snd, sdd;
-      int tnn, tdn, tnd, tdd;
-      int type;
-      scanf("%d", &type);
-      if (type != 2 || type != 3)
-        printf("Wrong type\n");
-      scanf("%d%d%d%d", &fnn, &fdn, &fnd, &fdd);
-      if (type == 2)
-      {
-        scanf("%d%d", &snn, &sdn);
-        //(snd/sdd) = (fnd/fdd)*(snn/sdn)/(fnn/fdn)
-        fnd *= snn;
-        fdd *= sdn;
-        fnd *= fdn;
-        fdd *= fnn;
-        int gcdval = gcd(fnd, fdd);
-        fnd /= gcdval;
-        fdd /= gcdval;
-        printf("%d/%d\n", fnd, fdd);
-      }
-
-      if (type == 3)
-      {
-        scanf("%d%d", &tnd, &tdd);
-        fnn *= tnd;
-        fdn *= tdd;
-        fnn *= fdd;
-        fdn *= fnd;
-        int gcdval = gcd(fnn, fdn);
-        fnn /= gcdval;
-        fdn /= gcdval;
-        printf("%d/%d\n", fnn, fdn);
-      }
-    }
-    
-    //add
-    if (!strcmp(action, "add"))
-    {
-      if (!strcmp(rdf, "d"))
-      {
-        int a, b;
-        scanf("%d%d", &a, &b);
-        printf("%d\n", a+b);
-      }
-      else if (!strcmp(rdf, "f"))
-      {
-        float a, b;
-        scanf("%f%f", &a, &b);
-        printf("%f\n", a+b);
-      }
-      else if (!strcmp(rdf, "lf"))
-      {
-        double a, b;
-        scanf("%lf%lf", &a, &b);
-        printf("%lf\n", a+b);
-      }
-      else if (!strcmp(rdf, "l"))
-      {
-        long double a, b;
-        scanf("%Lf%Lf", &a, &b);
-        printf("%Lf\n", a+b);
-      }
-      else
-        printf("check rdf\n");
-    }
-
-    //sub
-    if (!strcmp(action, "sub"))
-    {
-      if (!strcmp(rdf, "d"))
-        {
-          int a, b;
-          scanf("%d%d", &a, &b);
-          printf("%d\n", a-b);
-        }
-      else if (!strcmp(rdf, "f"))
-      {
-        int a, b;
-        scanf("%d%d", &a, &b);
-        printf("%d\n", a-b);
-      }
-      else if (!strcmp(rdf, "f"))
-      {
-        float a, b;
-        scanf("%d%d", &a, &b);
-        printf("%f\n", a-b);
-      }
-      else if (!strcmp(rdf, "lf"))
-      {
-        double a, b;
-        scanf("%lf%lf", &a, &b);
-        printf("%lf\n", a-b);
-      }
-      else if (!strcmp(rdf, "l"))
-      {
-        long double a, b;
-        scanf("%Lf%Lf", &a, &b);
-        printf("%Lf\n", a-b);
-      }
-      else
-        printf("check rdf\n");
-    }
-
-    //mul
-    if (!strcmp(action, "mul"))
-    {
-      if (!strcmp(rdf, "d"))
-      {
-        int a, b;
-        scanf("%d%d", &a, &b);
-        printf("%d\n", a*b);
-      }
-      else if (!strcmp(rdf, "f"))
-      {
-        float a, b;
-        scanf("%f%f", &a, &b);
-        printf("%f\n", a*b);
-      }
-      else if (!strcmp(rdf, "lf"))
-      {
-        double a, b;
-        scanf("%lf%lf", &a, &b);
-        printf("%lf\n", a*b);
-      }
-      else if (!strcmp(rdf, "l"))
-      {
-        long double a, b;
-        scanf("%Lf%Lf", &a, &b);
-        printf("%Lf\n", a*b);
-      }
-    }
-    
-    //div
-    if (!strcmp(action, "div"))
-    {
-      if (!strcmp(rdf, "d"))
-      {
-        int a, b;
-        scanf("%d%d", &a, &b);
-        printf("%d\n", a/b);
-      }
-      if (!strcmp(rdf, "f"))
-      {
-        float a, b;
-        scanf("%f%f", &a, &b);
-        printf("%f\n", a/b);
-      }
-      if (!strcmp(rdf, "lf"))
-      {
-        double a, b;
-        scanf("%lf%lf", &a, &b);
-        printf("%lf\n", a/b);
-      }
-      if (!strcmp(rdf, "l"))
-      {
-        long double a, b;
-        scanf("%Lf%Lf", &a, &b);
-        printf("%Lf\n", a/b);
-      }
-    }
-
-    //pow
-    if (!strcmp(action, "pow"))
-    {
-      if (!strcmp(rdf, "lf") || !strcmp(rdf, "d"))
-      {
-        double a, b;
-        scanf("%lf%lf", &a, &b);
-        printf("%lf\n", pow(a, b));
-      }
-      if (!strcmp(rdf, "f"))
-      {
-        float a, b;
-        scanf("%f%f", &a, &b);
-        printf("%f\n", powf(a, b));
-      }
-      if (!strcmp(rdf, "l"))
-      {
-        long double a, b;
-        scanf("%Lf%Lf", &a, &b);
+    if (!strcmp(action, "add") || !strcmp(action, "sub") ||
+        !strcmp(action, "mul") || !strcmp(action, "div") ||
+        !strcmp(action, "pow") || !strcmp(action, "rt") ||
+        !strcmp(action, "rod")) {
+      long double a, b;
+      scanf("%Lf%Lf", &a, &b);
+      if (!strcmp(action, "add"))
+        printf("%Lf\n", a + b);
+      if (!strcmp(action, "sub"))
+        printf("%Lf\n", a - b);
+      if (!strcmp(action, "mul"))
+        printf("%Lf\n", a * b);
+      if (!strcmp(action, "div"))
+        printf("%Lf\n", a / b);
+      if (!strcmp(action, "pow"))
         printf("%Lf\n", powl(a, b));
-
-      }
+      if (!strcmp(action, "rt"))
+        printf("%Lf\n", powl(a, 1 / b));
+      if (!strcmp(action, "rod"))
+        printf("%d\n", (int)a % (int)b);
     }
-
-    //rod
-    if (!strcmp(action, "rod"))
-    {
+    if (!strcmp(action, "gcd") || !strcmp(action, "lcm") ||
+        !strcmp(action, "rd") || !strcmp(action, "itm")) {
       int a, b;
       scanf("%d%d", &a, &b);
-      printf("%d\n", a%b);
-    }
-
-    //addfraction
-    if (!strcmp(action, "af")) 
-    {
-      int fn, fd, sn, sd;
-      scanf("%d%d%d%d", &fn, &fd, &sn, &sd);
-      fn = addfracnum(fn, fd, sn, sd);
-      fd = lcm(fd, sd);
-      int gcdval = gcd(fn, fd);
-      fn /= gcdval;
-      fd /= gcdval;
-      printf("%d/%d\n", fn, fd);
-    }
-
-    //subfraction
-    if (!strcmp(action, "sf"))
-    {
-      int fn, fd, sn, sd;
-      scanf("%d%d%d%d", &fn, &fd, &sn, &sd);
-      fn = subfracnum(fn, fd, sn, sd);
-      fd = lcm(fd, sd);
-      int gcdval = gcd(fn, fd);
-      fn /= gcdval;
-      fd /= gcdval;
-      printf("%d/%d\n", fn, fd);
-    }
-
-    //mulfraction
-    if (!strcmp(action, "mf"))
-    {
-      int fn, fd, sn, sd;
-      scanf("%d%d%d%d", &fn, &fd, &sn, &sd);
-      fn *= sn;
-      fd *= sd;
-      int gcdval = gcd(fn, fd);
-      fn /= gcdval;
-      fd /= gcdval;
-      printf("%d/%d\n", fn, fd);
-    }
-
-    //divfraction
-    if (!strcmp(action, "df"))
-    {
-      int fn, fd, sn, sd;
-      scanf("%d%d%d%d", &fn, &fd, &sn, &sd);
-      fn *= sd;
-      fd *= sn;
-      int gcdval = gcd(fn, fd);
-      fn /= gcdval;
-      fd /= gcdval;
-      printf("%d/%d\n", fn, fd);
-    }
-  
-    //fd
-    if (!strcmp(action, "fd"))
-    {
-      int number;
-      scanf("%d", &number);
-      for (int i = 1; i <= number; i++)
-      {
-        if (number % i == 0)
-        {
-          printf("%d ", i);
-        }
+      if (!strcmp(action, "gcd"))
+        printf("%d\n", gcd(a, b));
+      if (!strcmp(action, "lcm"))
+        printf("%d\n", lcm(a, b));
+      if (!strcmp(action, "rd")) {
+        int gcdval = gcd(a, b);
+        printf("%d/%d %d\n", a / gcdval, b / gcdval, gcdval);
       }
-      printf("\n");
+      if (!strcmp(action, "itm")) {
+        int fp = a / b;
+        a %= b;
+        printf("%d %d/%d\n", fp, a, b);
+      }
     }
-
-    //tdap
-    if (!strcmp(action, "tdap"))
-    {
-      int num, denom;
-      scanf("%d%d", &num, &denom);
-      printf("%lf\n", (double)num/(double)denom);
-      printf("%lf%%\n", (double)num/((double)denom/100));
+    if (!strcmp(action, "tif") || !strcmp(action, "tbd")) {
+      if (!strcmp(action, "tif")) {
+        int fp, num, denom;
+        scanf("%d%d%d", &fp, &num, &denom);
+        num = denom * fp + num;
+        printf("%d/%d\n", num, denom);
+      }
+      if (!strcmp(action, "tbd")) {
+        long double num, denom, decimal;
+        scanf("%Lf%Lf%Lf", &num, &denom, &decimal);
+        printf("%Lf\n", num / (denom / decimal));
+      }
     }
-
-    if (!strcmp(action, "tif"))
-    {
-      int num, denom, fp;
-      scanf("%d%d%d", &fp, &num, &denom);
-      num = denom * fp + num;
-      printf("%d/%d", num, denom);
+    if (!strcmp(action, "af") || !strcmp(action, "sf") ||
+        !strcmp(action, "mf") || !strcmp(action, "df")) {
+      int fn, fd, sn, sd;
+      scanf("%d%d%d%d", &fn, &fd, &sn, &sd);
+      if (!strcmp(action, "af") || !strcmp(action, "sf")) {
+        int cd = lcm(fd, sd), gcdval;
+        fn *= cd / fd;
+        sn *= cd / sd;
+        if (!strcmp(action, "af"))
+          fn += sn;
+        else
+          fn -= sn;
+        gcdval = gcd(fn, cd);
+        printf("%d/%d %d\n", fn, cd, gcdval);
+      }
+      if (!strcmp(action, "mf") || !strcmp(action, "df")) {
+        if (!strcmp(action, "mf")) {
+          fn *= sn;
+          fd *= sd;
+        } else {
+          fn *= sd;
+          fd *= sn;
+        }
+        int gcdval = gcd(fn, fd);
+        printf("%d/%d %d\n", fn, fd, gcdval);
+      }
     }
-
-    if (!strcmp(action, "itm"))
-    {
-      int num, denom, fp;
-      scanf("%d%d", &num, &denom);
-      fp = num/denom;
-      num %= denom;
-      printf("%d %d/%d\n", fp, num, denom);
-    }
-
-    if (!strcmp(action, "dtf"))
-    {
-      int num, denom;
-      const int percision = 1000000;
+    if (!strcmp(action, "dtf")) {
+      int num, denom = 1000000, gcdval;
       double decimal;
       scanf("%lf", &decimal);
-      
-      num = (int)(decimal * percision);
-      denom = percision;
-      int gcdval = gcd(num, denom);
+      num = (int)(decimal * denom);
+      gcdval = gcd(num, denom);
       num /= gcdval;
       denom /= gcdval;
       printf("%d/%d\n", num, denom);
     }
-
-    if (!strcmp(action, "reduce"))
-    {
-      int num, denom;
-      scanf("%d%d", &num, &denom);
-      int gcdval = gcd(num, denom);
-      num /= gcdval;
-      denom /= gcdval;
-      printf("%d/%d\n", num, denom);
-      
-    }
-
-    if (!strcmp(action, "rt"))
-    {
-      char type[2];
-      scanf("%s", type);
-      if (!strcmp(type, "lf"))
-      {
-        double a, b;
-        scanf("%lf%lf", &a, &b);
-        printf("%lf\n", pow(a, 1/b));
-      }
-      if (!strcmp(type, "f"))
-      {
-        float a, b;
-        scanf("%f%f", &a, &b);
-        printf("%f\n", pow(a, 1/b));
-      }
-      if (!strcmp(type, "l"))
-      {
-        long double a, b;
-        scanf("%Lf%Lf", &a, &b);
-        printf("%Lf\n", pow(a, 1/b));
-      }
-    }
-
-    if (!strcmp(action, "lcm"))
-    {
-      int a, b;
-      scanf("%d%d", &a, &b);
-      printf("%d\n", lcm(a, b));
-    }
-
-    if (!strcmp(action, "gcd"))
-    {
-      int a, b;
-      scanf("%d%d", &a, &b);
-      printf("%d\n", gcd(a,b));
+    if (!strcmp(action, "cmpf")) {
+      int fn, fd, sn, sd;
+      scanf("%d%d%d%d", &fn, &fd, &sn, &sd);
+      double ff = (double)fn / (double)fd, sf = (double)sn / (double)sd;
+      if (ff > sf)
+        printf("%d/%d > %d/%d\n", fn, fd, sn, sd);
+      if (ff < sf)
+        printf("%d/%d < %d/%d\n", fn, fd, sn, sd);
+      if (ff == sf)
+        printf("%d/%d = %d/%d\n", fn, fd, sn, sd);
     }
   }
 }
