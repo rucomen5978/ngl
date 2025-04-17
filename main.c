f83261ba45dd3e2c9b03e93be72e81268070b109
rucomen5978 <121585255+rucomen5978@users.noreply.github.com>
2025-04-17 18:03:47 +0300

    Update main.c

--- main.c
+++ main.c
@@ -16,8 +16,6 @@ int lcm(int a, int b) { return a * b / gcd(a, b); }
 
 int main() {
   char action[256];
-  int fn, fd, sn, sd, gcdval, cd;
-  long double a, b, size;
   while (1) {
     scanf("%s", action);
 
@@ -29,82 +27,97 @@ int main() {
       system("clear");
 
     // Base actions
-    if (!strcmp(action, "add")) {:
-      scanf("%Lf%Lf", &a, &b);
-      printf("%Lf\n", a + b);
+    if (!strcmp(action, "add")) {
+      long double fn, sn;
+      scanf("%Lf%Lf", &fn, &sn);
+      printf("%Lf\n", fn + sn);
     }
 
     if (!strcmp(action, "sub")) {
-      scanf("%Lf%Lf", &a, &b);
-      printf("%Lf\n", a - b);
+      long double fn, sn;
+      scanf("%Lf%Lf", &fn, &sn);
+      printf("%Lf\n", fn - sn);
     }
 
     if (!strcmp(action, "mul")) {
-      scanf("%Lf%Lf", &a, &b);
-      printf("%Lf\n", a * b);
+      long double fn, sn;
+      scanf("%Lf%Lf", &fn, &sn);
+      printf("%Lf\n", fn * sn);
     }
 
     if (!strcmp(action, "div")) {
-      scanf("%Lf%Lf", &a, &b);
-      printf("%Lf\n", a / b);
+      long double fn, sn;
+      scanf("%Lf%Lf", &fn, &sn);
+      printf("%Lf\n", fn / sn);
     }
 
     if (!strcmp(action, "rod")) {
+      int fn, sn;
       scanf("%d%d", &fn, &sn);
       printf("%d\n", fn % sn);
     }
 
     if (!strcmp(action, "gcd")) {
+      int fn, sn;
       scanf("%d%d", &fn, &sn);
       printf("%d\n", gcd(fn, sn));
     }
 
     if (!strcmp(action, "lcm")) {
+      int fn, sn;
       scanf("%d%d", &fn, &sn);
       printf("%d\n", lcm(fn, sn));
     }
 
     if (!strcmp(action, "pow")) {
-      scanf("%Lf%Lf", &a, &b);
-      printf("%Lf\n", powl(a, b));
+      long double fn, sn;
+      scanf("%Lf%Lf", &fn, &sn);
+      printf("%Lf\n", powl(fn, sn));
     }
 
     if (!strcmp(action, "cos")) {
-      scanf("%Lf", &a);
-      printf("%Lf\n", cosl(a));
+      long double num;
+      scanf("%Lf", &num);
+      printf("%Lf\n", cosl(num));
     }
 
     if (!strcmp(action, "sin")) {
-      scanf("%Lf", &a);
-      printf("%Lf\n", sinl(a));
+      long double num;
+      scanf("%Lf", &num);
+      printf("%Lf\n", sinl(num));
     }
 
-    if (!strcmp(action, "tan")) {
-      scanf("%Lf", &a);
-      printf("%Lf\n", tanl(a));
+    if (!strcmp(action, "tan"))/home/bratik/dwm/ {
+      long double num;
+      scanf("%Lf", &num);
+      printf("%Lf\n", tanl(num));
     }
 
     // Fraction part
     if (!strcmp(action, "rd")) {
+      int fn, sn, gcdval;
       scanf("%d%d", &fn, &sn);
       gcdval = gcd(fn, sn);
       printf("%d/%d %d\n", fn / gcdval, sn / gcdval, gcdval);
     }
 
     if (!strcmp(action, "itm")) {
-      scanf("%d%d", &fn, &fd);
-      sn = fn / fd;
-      fn %= fd;
-      printf("%d %d/%d\n", sn, fn, fd);
+      int fp, num, denom;
+      scanf("%d%d", &num, &denom);
+      fp = num / denom;
+      num %= denom;
+      printf("%d %d/%d\n", fp, num, denom);
     }
 
     if (!strcmp(action, "tif")) {
-      scanf("%d%d%d", &sn, &fn, &fd);
-      fn = fd * sn + fn;
-      printf("%d/%d\n", fn, fd);
+      int fp, num, denom;
+      scanf("%d%d%d", &fp, &num, &denom);
+      num = denom * fp + num;
+      printf("%d/%d\n", num, denom);
     }
 
     if (!strcmp(action, "af") || !strcmp(action, "sf")) {
+      int fn, fd, sn, sd, cd, gcdval;
       scanf("%d%d%d%d", &fn, &fd, &sn, &sd);
       cd = lcm(fd, sd);
       fn *= cd / fd;
@@ -118,6 +131,7 @@ int main() {
     }
 
     if (!strcmp(action, "mf") || !strcmp(action, "df")) {
+      int fn, fd, sn, sd, cd, gcdval;
       scanf("%d%d%d%d", &fn, &fd, &sn, &sd);
       if (!strcmp(action, "mf")) {
         fn *= sn;
@@ -131,13 +145,15 @@ int main() {
     }
 
     if (!strcmp(action, "dtf")) {
-      fd = 1e6;
-      scanf("%Lf", &a);
-      fn = (int)(a * fd);
-      gcdval = gcd(fn, fd);
-      fn /= gcdval;
-      fd /= gcdval;
-      printf("%d/%d\n", fn, fd);
+      int num, denom, gcdval;
+      long double decimal;
+      denom = 1e6;
+      scanf("%Lf", &decimal);
+      num = (int)(decimal * denom);
+      gcdval = gcd(num, denom);
+      num /= gcdval;
+      denom /= gcdval;
+      printf("%d/%d\n", num, denom);
     }
   }
   return 0;
